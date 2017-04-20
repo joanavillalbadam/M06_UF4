a98cd41f5002ae56481aaa42832ea25a49672db5
ALUMNEDAM <ALUMNEDAM@192.168.120.71>
2017-04-20 19:45:55 +0200

    Merge origin/master
    Conflicts:
    	prueba2/src/prueba2/Prueba2.java

---
+++ Proyecto_M06_UF4/src/controlador/Alumne_control.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import Interficies.AlumneIn;
+
+/**
+ *
+ * @author ALUMNEDAM
+ */
+public class Alumne_control implements AlumneIn{
+
+    @Override
+    public void buscar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Afegir(Object t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void eliminar(Object t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void modificar(Object t, Object nuevoT) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
